116d701b3b36a796dccae294051256918933ebd0
Yaungad Yasaway <yasaway@mail.ru>
2022-12-06 00:16:19 +0500

    Задача 29 отредактирована

--- task29/Program.cs
+++ task29/Program.cs
@@ -8,7 +8,6 @@ int[] array = new int[8];
 Random rnd = new Random();
 for (int i = 0; i < array.Length; i++)
 {
-    int xrand = rnd.Next(0, 100);
-    array[i] = xrand;
+    array[i] = rnd.Next(0, 100);
     Console.Write(array[i] + " ");
 }
