9b746aca57977735fb1ba8897f190330543ee8d7
Yaungad Yasaway <yasaway@mail.ru>
2022-12-05 22:05:35 +0500

    ДЗ 4 урока

---
+++ task25/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 25: Напишите цикл, который принимает 
+// на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3?)
+// 2, 4 -> 16
+
+Console.Write("Ведите число А: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ведите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+int VozvestiVB(int numA, int numB)
+{
+    int vozvb = numA;
+    for (int i = 1; i < numB; i++)
+    {
+        vozvb = vozvb * numA; 
+    }
+    return vozvb;
+}
+
+if (A != 0 && B > 0)
+{
+    int C = VozvestiVB(A, B);
+    Console.WriteLine($"Число {A} в степени {B} равно {C}");
+}
+else System.Console.WriteLine("Требуется ввести натуральное число!");
---
+++ task27/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 27: Напишите программу, которая 
+// принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Write("Введите число: ");
+int chislo = Convert.ToInt32(Console.ReadLine());
+
+int SummaCifr(int x)
+{
+    x = Math.Abs(x);
+    int xsum = 0;
+    while (x > 0)
+    {
+        int xchiclo = x % 10;
+        xsum = xsum + xchiclo;
+        x = x / 10;
+    }
+    return xsum;
+}
+
+int summaCifr = SummaCifr(chislo);
+Console.WriteLine($"Сумма цифр числа {chislo} равна {summaCifr}");
