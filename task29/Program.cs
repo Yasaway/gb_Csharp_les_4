fb9e28f0e0adcf07b12a144a5109ff285a4351d2
Yaungad Yasaway <yasaway@mail.ru>
2022-12-06 13:26:49 +0500

    Задача 29 доделана

--- task29/Program.cs
+++ task29/Program.cs
@@ -3,11 +3,27 @@
 
 // 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
 
-Console.WriteLine("Программа выгружает массив из 8 цифр ");
-int[] array = new int[8];
-Random rnd = new Random();
-for (int i = 0; i < array.Length; i++)
+void packArray(int[] xarray)
 {
-    array[i] = rnd.Next(0, 100);
-    Console.Write(array[i] + " ");
+    Random rnd = new Random();
+    for (int i = 0; i < xarray.Length; i++)
+    {
+        xarray[i] = rnd.Next(0, 100);
+    }
 }
+
+void printArray(int[] narray)
+{
+    for (int i = 0; i < narray.Length; i++)
+    {
+        Console.Write(narray[i] + " ");
+    }
+}
+ 
+Console.WriteLine("Программа выгружает массив из 8 цифр ");
+int lengthMass = 8;
+int[] array = new int[lengthMass];
+
+packArray(array);
+
+printArray(array);
